13035bf5cd23d4d9f8fb33b31f407ca54090a8e7
gukai <3244255376@qq.com>
2024-01-02 16:48:04 +0800

    feat: 移动端适配

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,53 +1,151 @@
 <template>
-  <a-layout>
-    <div class="logo">
-      <a
-        href="https://opendrivelab.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-        style="display: flex; justify-content: center; align-items: center"
-      >
-        <img src="../assets/OpenDriveLab.png" alt="Logo"
-      /></a>
+  <div style="width: 100%">
+    <a-layout v-if="screenWidth>=600">
+      <div class="logo">
+        <a
+          href="https://opendrivelab.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style="display: flex; justify-content: center; align-items: center"
+        >
+          <img src="../assets/OpenDriveLab.png" alt="Logo"
+        /></a>
+      </div>
+      <a-layout-header class="layout-header">
+        <a-menu mode="horizontal">
+          <a-menu-item>
+            <router-link to="/">
+              <a-icon type="database" />AD-Datasets</router-link
+            >
+          </a-menu-item>
+          <a-menu-item key="Sponsor">
+            <a
+              href="https://github.com/sponsors/OpenDriveLab"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <a-icon
+                type="heart"
+                theme="filled"
+                style="color: rgba(220, 20, 60, 1)"
+              />Sponsor</a
+            >
+          </a-menu-item>
+        </a-menu>
+      </a-layout-header>
+    </a-layout>
+    <div class="mobile-class" v-else>
+      <div class="logo">
+        <a
+          href="https://opendrivelab.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style="display: flex; justify-content: center; align-items: center"
+        >
+          <img src="../assets/OpenDriveLab.png" alt="Logo"
+        /></a>
+      </div>
+      <div style="width: 180px" class="mobile-class-menu">
+        <a-button
+          type="primary"
+          style="margin-bottom: 16px; margin-left: 120px; width: 45px"
+          @click="toggleCollapsed"
+        >
+          <a-icon :type="collapsed ? 'menu-unfold' : 'menu-fold'" />
+        </a-button>
+        <a-menu
+          :default-selected-keys="['database']"
+          :default-open-keys="['sub1']"
+          mode="inline"
+          theme="light"
+          :inline-collapsed="collapsed"
+          v-show="menuShow"
+        >
+          <a-menu-item key="database">
+            <router-link to="/">
+              <a-icon type="database" />AD-Datasets</router-link
+            >
+          </a-menu-item>
+          <a-menu-item key="Sponsor">
+            <a
+              href="https://github.com/sponsors/OpenDriveLab"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <a-icon
+                type="heart"
+                theme="filled"
+                style="color: rgba(220, 20, 60, 1)"
+              />Sponsor</a
+            >
+          </a-menu-item>
+          <!-- <a-menu-item key="1" >
+            <a-icon type="pie-chart" />
+            <span>Option 1</span>
+          </a-menu-item>
+          <a-menu-item key="2" >
+            <a-icon type="desktop" />
+            <span>Option 2</span>
+          </a-menu-item> -->
+        </a-menu>
+      </div>
     </div>
-    <a-layout-header class="layout-header">
-      <a-menu mode="horizontal">
-        <a-menu-item>
-          <!-- <a href="" rel="noopener noreferrer">
-            <a-icon type="database" />AD-Datasets
-          </a> -->
-          <router-link to="/">
-            <a-icon type="database" />AD-Datasets</router-link
-          >
-        </a-menu-item>
-        <a-menu-item key="Sponsor">
-          <a
-            href="https://github.com/sponsors/OpenDriveLab"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <!-- <a-icon type="heart" shape="solid" color="#FF0000" /> -->
-            <a-icon
-              type="heart"
-              theme="filled"
-              style="color: rgba(220, 20, 60, 1)"
-            />Sponsor</a
-          >
-        </a-menu-item>
-      </a-menu>
-    </a-layout-header>
-  </a-layout>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      screenWidth: document.body.clientWidth,
+      collapsed: true,
+      menuShow: false,
+    };
+  },
+  created() {
+    // console.log("this.screenWidth===================", this.screenWidth);
+  },
+  mounted() {
+    window.onresize = () => {
+      this.screenWidth = document.body.clientWidth;
+      // console.log("this.screenWidth===================", this.screenWidth);
+    };
+  },
+  watch: {
+    screenWidth(newValue) {
+      // 为了避免频繁触发resize函数导致页面卡顿，使用定时器
+      if (!this.timer) {
+        // 一旦监听到的screenWidth值改变，就将其重新赋给data里的screenWidth
+        this.screenWidth = newValue;
+        this.timer = true;
+        setTimeout(() => {
+          //console.log(this.screenWidth);
+          this.timer = false;
+        }, 400);
+      }
+    },
+  },
+  methods: {
+    toggleCollapsed() {
+      this.collapsed = !this.collapsed;
+      this.menuShow = !this.menuShow;
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+.mobile-class {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  padding: 20px;
+}
+.mobile-class-menu {
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+}
 .ant-menu-horizontal {
   line-height: 65px;
   white-space: nowrap;
